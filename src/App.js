56083236c65ee2b0785397f5086eb3c3927e78b2
Dennis Baldwin <dennisbaldwin@gmail.com>
2021-05-07 09:41:51 -0500

    Working with state

--- src/App.js
+++ src/App.js
@@ -5,12 +5,34 @@ import './App.css';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.setState({
+      activeTab: "Mission 1",
+      currentCode: null,
+      code1: null,
+      code2: null,
+      code3: null,
+      code4: null,
+      code5: null
+    });
+
+  }
+
   printActiveTab(tab) {
     console.log(tab);
+    this.setState({
+      activeTab: tab
+    });
   }
 
   printCode(code) {
+
     console.log("From app.js " + code);
+    this.setState({
+      currentCode: code
+    });
   }
 
   render() {
@@ -18,26 +40,26 @@ class App extends Component {
       <div>
         <h1>Tabs Demo</h1>
         
-        <Tabs onTabChanged={this.printActiveTab}> 
-          <div label="Mission #1"> 
+        <Tabs onTabChanged={this.printActiveTab.bind(this)}> 
+          <div label="Mission 1"> 
             See ya later, <em>Alligator</em>! 
           </div> 
-          <div label="Mission #2"> 
+          <div label="Mission 2"> 
             After 'while, <em>Crocodile</em>! 
           </div> 
-          <div label="Mission #3"> 
+          <div label="Mission 3"> 
             Nothing to see here, this tab is <em>extinct</em>! 
           </div>
-          <div label="Mission #4"> 
+          <div label="Mission 4"> 
             Nothing to see here, this tab is <em>extinct</em>! 
           </div> 
-          <div label="Mission #5"> 
+          <div label="Mission 5"> 
             Nothing to see here, this tab is <em>extinct</em>! 
           </div> 
         </Tabs>
 
         
-        <BlocklyCanvas handleWorkspaceChanged={this.printCode} />
+        <BlocklyCanvas handleWorkspaceChanged={this.printCode.bind(this)} />
         
       </div>
     );
